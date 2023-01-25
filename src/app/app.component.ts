982fdcd3346d5f056aebf5388a683d1fe17411e2
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2023-01-25 17:34:26 +0200

    attempt to solve task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {delay, map, Observable, merge, concatWith, combineAll, concatAll, tap} from "rxjs";
+import { of } from "rxjs"
 
 @Component({
     selector: 'my-app',
-    template: `<label>Введите имя:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Добро пожаловать {{name}}!</h1>`
+    template: ``,
 })
-export class AppComponent {
-    name= '';
+export class AppComponent implements OnInit {
+    sendRequest(number: number): Observable<number> {
+        console.log("in send request with parameter " + number);
+        const timeDelay:number = 200;
+        const addValue:number = 100;
+
+        return of(number + addValue).pipe(delay(timeDelay));
+    }
+
+    getResponse(request: Observable<number>): Observable<number> {
+        let collectionNumber:number[] = [];
+        request.pipe(map(x => this.sendRequest(x))).subscribe( p => p.subscribe(n => {
+            collectionNumber.push(n);
+        }));
+        return of(...collectionNumber);
+    }
+
+    ngOnInit() {
+        const response:Observable<number> = this.getResponse(of(1, 2, 3, 4, 5));
+        response.subscribe(x => console.log(x));
+    }
+
 }
