b3a0c7c3735544d9b1765e41c4d0421e704d8e25
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2023-01-25 22:40:18 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {delay, map, Observable, merge, concatWith, combineAll, concatAll, tap} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import {delay, map, Observable, Subject} from "rxjs";
 import { of } from "rxjs"
 
 @Component({
@@ -8,24 +8,29 @@ import { of } from "rxjs"
 })
 export class AppComponent implements OnInit {
     sendRequest(number: number): Observable<number> {
-        console.log("in send request with parameter " + number);
-        const timeDelay:number = 200;
+        const timeDelay:number = 500;
         const addValue:number = 100;
 
         return of(number + addValue).pipe(delay(timeDelay));
     }
 
     getResponse(request: Observable<number>): Observable<number> {
-        let collectionNumber:number[] = [];
-        request.pipe(map(x => this.sendRequest(x))).subscribe( p => p.subscribe(n => {
-            collectionNumber.push(n);
-        }));
-        return of(...collectionNumber);
+        const result: Observable<number> =  new Observable<number>(observer => {
+            request.pipe(map(num => this.sendRequest(num)))
+                .subscribe(obs => obs.subscribe(
+                    value => observer.next(value)
+                ))
+        });
+
+        return result;
     }
 
     ngOnInit() {
-        const response:Observable<number> = this.getResponse(of(1, 2, 3, 4, 5));
-        response.subscribe(x => console.log(x));
+        const testObservable:Observable<number> = of(1, 2, 3, 4, 5);
+        testObservable.subscribe(x => console.log(`begin value: ${x}`));
+
+        const response:Observable<number> = this.getResponse(testObservable);
+        response.subscribe(x => console.log(`end value: ${x}`));
     }
 
 }
