42fb88be1b2900dcc92c2fe4555d79cb538eb51e
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2023-01-26 17:28:02 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {delay, map, Observable } from "rxjs";
-import { of } from "rxjs"
+import {Component, OnInit} from '@angular/core';
+import {delay, forkJoin, map, mergeMap, Observable, scan, switchAll, switchMap, takeLast, tap} from "rxjs";
+import {of} from "rxjs"
 
 @Component({
     selector: 'my-app',
@@ -8,28 +8,29 @@ import { of } from "rxjs"
 })
 export class AppComponent implements OnInit {
     sendRequest(number: number): Observable<number> {
-        const timeDelay:number = 500;
-        const addValue:number = 100;
+        const addValue: number = 100;
+        const timeDelay: number = 2000;
 
-        return of(number + addValue).pipe(delay(timeDelay));
+        return of(number + addValue)
+            .pipe(delay(timeDelay));
     }
 
-    getResponse(request: Observable<number>): Observable<number> {
-        const result$: Observable<number> =  new Observable<number>(observer => {
-            request.pipe(map(num => this.sendRequest(num)))
-                .subscribe(obs$ => obs$.subscribe(
-                    value => observer.next(value)
-                ))
-        });
+    getResponse(request$: Observable<number>): Observable<number[]> {
+        const result$: Observable<number[]> = request$
+            .pipe(
+                mergeMap(x => this.sendRequest(x)),
+                scan((acc: number[], cur: number) => [...acc, cur], []),
+                takeLast(1)
+            );
 
         return result$;
     }
 
     ngOnInit() {
-        const testObservable$:Observable<number> = of(1, 2, 3, 4, 5);
+        const testObservable$: Observable<number> = of(1, 2, 3, 4, 5);
         testObservable$.subscribe(x => console.log(`begin value: ${x}`));
 
-        const response$:Observable<number> = this.getResponse(testObservable$);
+        const response$: Observable<number[]> = this.getResponse(testObservable$);
         response$.subscribe(x => console.log(`end value: ${x}`));
     }
 }
